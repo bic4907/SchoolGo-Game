48b047e511426f590cffeedfca4a0d95bbd17f1e
Baek In-Chang <bic4907@gmail.com>
2018-12-08 17:50:52 +0900

    Update Result.java

--- src/Result.java
+++ src/Result.java
@@ -5,7 +5,7 @@ public class Result {
 	public ArrayList<String> desc;
 	
 	public Result() {
-		type = null;
-		desc = new ArrayList<String>();
+		type = null;			// 엔딩타입 Happy, sad...
+		desc = new ArrayList<String>(); // 각 단과대학별 상태를 담고있는 ArrayList
 	}
 }
